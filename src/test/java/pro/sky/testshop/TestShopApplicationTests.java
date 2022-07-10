9a439fb9e9aadbafec2a0cd22f418abf6ab28624
m-kac <m-kachalov@mail.ru>
2022-07-10 20:04:50 +0300

    Initial commit

---
+++ src/test/java/pro/sky/testshop/TestShopApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.testshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
